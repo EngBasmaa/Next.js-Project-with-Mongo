0ab58fc734833b1c670229241c27eb7eee9338a7
EngBasmaa <basmaelmasry263@gmail.com>
2025-05-23 23:37:53 +0300

    updates

---
+++ src/app/about/mision/page.js
@@ -0,0 +1,51 @@
+
+import React from 'react'
+
+export default function Mision() {
+  return (
+    <>
+     <div className="pb-5 mx-0 page">
+          {/* Heading */}
+          <div className="text-center" style={{ marginBottom: "5rem" }}>
+            <img
+              src="/freelance-young-asian-businesswoman-casual-wear-using-laptop-working-living-room-home.jpg"
+              alt="banner"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                marginBottom: "3rem"
+              }}
+            />
+            <h2
+              style={{
+                color: "var(--primary)",
+                fontWeight: "bold",
+                marginBottom: "3rem",
+              backgroundColor: "var(--accent)",
+                padding: "0.5rem"
+              }}
+            >
+              Our Mision
+            </h2>
+            <p
+              style={{
+                color: "var(--secondary)",
+                maxWidth: "600px",
+                margin: "10px auto",
+                marginBottom: "3rem",
+                lineHeight: "2rem"
+              }}
+            >
+              We are a passionate web development team led by Basma Elmasry,
+              specialized in building fast, responsive, and beautiful web
+              applications using modern technologies like Next.js, React, and
+              Bootstrap.
+            </p>
+          </div>
+    
+         
+        </div>
+    </>
+  )
+}
--- src/components/sideMenu/index.js
+++ src/components/sideMenu/index.js
@@ -2,15 +2,15 @@ import Link from "next/link";
 import React from "react";
 
 export default function SideMenu() {
-  return <aside className="w-48 min-h-screen bg-gray-100 p-4 shadow-md">
+  return <aside className="w-48 min-h-screen  bg-gray-100 p-5 h-100 shadow-md" style={{ backgroundColor: "var(--accent)" }}>
       <nav className="flex flex-col space-y-4">
-        <Link className="nav-link text-blue-700 hover:text-blue-900 transition-colors duration-300" href="/about">
+        <Link className="nav-link mb-5 text-blue-700 transition-colors p-2 rounded duration-300" style={{ border: "2px solid var(--primary)" }} href="/about">
           About
         </Link>
-        <Link className="nav-link text-blue-700 hover:text-blue-900 transition-colors duration-300" href="/about/mision">
+        <Link className="nav-link mb-5 text-blue-70 transition-colors p-2 rounded duration-300" style={{ border: "2px solid var(--primary)" }} href="/about/mision">
           Mision
         </Link>
-        <Link className="nav-link text-blue-700 hover:text-blue-900 transition-colors duration-300" href="/about/vision">
+        <Link className="nav-link text-blue-700  transition-colors p-2 rounded duration-300" style={{ border: "2px solid var(--primary)" }} href="/about/vision">
           Vision
         </Link>
       </nav>
